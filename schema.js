81139ef71bd7eeea6d39ac4ba9e27249c1c631bc
Moses Nova <mosesnova@gmail.com>
2019-12-09 15:07:42 +1300

    Initial commit

---
+++ schema.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+const {GraphQLObjectType, 
+    GraphQLInt, GraphQLString, GraphQLBoolean, GraphQLList,
+GraphQLSchema} = require('graphql');
+
+
+//Launch Type
+const LaunchType = new GraphQLObjectType({
+    name:'Launch',
+    fields:()=>({
+        flight_number: { type:GraphQLInt},
+        mission_name: { type:GraphQLString},
+        lauch_year: { type:GraphQLString},
+        launch_date_local: { type:GraphQLInt},
+        launch_success: { type:GraphQLBoolean},
+        rocket: { type:RocketType}
+    })
+});
+const RocketType = new GraphQLObjectType({
+    name:'Rocket',
+    fields:()=>({
+        rocket_id: { type:GraphQLString},
+        rocket_name: { type:GraphQLString},
+        rocket_type: { type:GraphQLString}
+    })
+});
+
+//Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields:{
+        launches:
+        {
+            type:new GraphQLList(LaunchType),
+            resolve(parent,args){
+                    return axios.get('https://api.spacexdata.com/v3/launches')
+                    .then(res => res.data);
+            }
+        },
+        launch:{
+            type: LaunchType,
+            args:{
+                flight_number:{ type: GraphQLInt},
+                mission_name:{type: GraphQLString}
+            },
+            resolve(parent,args){
+                return axios.get('https://api.spacexdata.com/v3/launches/${args.flight_number}')
+                .then(res => res.data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+})
